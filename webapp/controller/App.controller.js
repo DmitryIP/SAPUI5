9b307b4afbad97a770b0659eb24f8a0a2457462d
DmitryIP <dmitryip@tut.by>
2021-11-29 23:39:51 +0300

    Step 8: Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,31 +4,31 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel"
 ], function (Controller, MessageToast, JSONModel, ResourceModel) {
-    "use strict"
-    return Controller.extend("sap.ui.demo.walkthrough.App", {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
         onInit: function () {
-            // det the data model on the view
+            // set data model on view
             var oData = {
                 recipient: {
-                    name: "UI5"
+                    name: "World"
                 }
             };
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
-            //set i18 model
+            // set i18n model on view
             var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.walkthrough. i18n.i18n",
-                supportedLocales: ["en"],
-                fallbaskLocale:""
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+                // supportedLocales: [""],
+                // fallbaskLocale: ""
             });
-            this.getView().setModel(i18nModel, "i18n")  
-                    },
+            this.getView().setModel(i18nModel, "i18n");
+        },
         onShowHello: function () {
             // read msg from i18n model
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
             var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient])
-            //show message
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            // show message
             MessageToast.show(sMsg);
         }
     });
