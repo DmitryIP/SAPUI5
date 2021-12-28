e2630140f58166bd5a818954e456a205eb1789e8
DmitryIP <dmitryip@tut.by>
2021-12-28 22:24:35 +0300

    Step 32: Routing Back and History

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,13 +1,14 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
 	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+], function (Controller, History, JSONModel) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
 		onInit: function () {
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
-			
+
 			//-----
 			var oDetailModel = new JSONModel({
 				currency: "EUR"
@@ -20,6 +21,18 @@ sap.ui.define([
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
 			});
+		},
+
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
 		}
 	});
 });
