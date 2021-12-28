bee031c253fb8d27d3443a7ed48c290085a76ae5
DmitryIP <dmitryip@tut.by>
2021-12-28 22:06:54 +0300

    Step 31: Routing with Parameters

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
+		onInit: function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+			
+			//-----
+			var oDetailModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oDetailModel, "detail");
+			//----
+		},
+		_onObjectMatched: function (oEvent) {
+			this.getView().bindElement({
+				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+				model: "invoice"
+			});
+		}
+	});
+});
